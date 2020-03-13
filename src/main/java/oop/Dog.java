c62b6c92bf0ed89a5f822b10e159ffca04e8a5f5
Kirill Malikov <makige.ee@mail.ru>
2020-03-13 21:41:49 +0200

    1.1. Создание объекта[#244560]

--- src/main/java/oop/Dog.java
+++ src/main/java/oop/Dog.java
@@ -2,6 +2,8 @@ package oop;
 
 public class Dog {
     public static void main(String[] args) {
-        Dog polkan, sharik, zhychka = new Dog();
+        Dog polkan = new Dog();
+        Dog sharik  = new Dog();
+        Dog zhychka = new Dog();
     }
 }
