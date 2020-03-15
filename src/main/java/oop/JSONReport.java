6a02d42d9ed89b3e9bd4b2274075f8a779f2189f
Kirill Malikov <makige.ee@mail.ru>
2020-03-15 15:51:25 +0200

    4. Переопределение[#244573]

--- src/main/java/oop/JSONReport.java
+++ src/main/java/oop/JSONReport.java
@@ -3,6 +3,6 @@ package oop;
 public class JSONReport extends TextReport {
     @Override
     public String generate(String name, String body) {
-        return "name: " + name + '\n' + "body: " + body;
+        return "name: " + name + System.lineSeparator() + "body: " + body;
     }
 }
