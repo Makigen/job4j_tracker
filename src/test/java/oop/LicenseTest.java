d100cfa9bcbba8b679cd4ecea89b83616a049bfb
Kirill Malikov <makige.ee@mail.ru>
2020-03-15 20:36:56 +0200

    4. Сравнение моделей. Метод equals.[#244580]

---
+++ src/test/java/oop/LicenseTest.java
@@ -0,0 +1,17 @@
+package oop;
+
+import static org.junit.Assert.*;
+import static org.hamcrest.CoreMatchers.*;
+import org.junit.Test;
+
+public class LicenseTest {
+
+    @Test
+    public void eqName() {
+        License first = new License();
+        first.setCode("audio");
+        License second = new License();
+        second.setCode("audio");
+        assertThat(first, is(second));
+    }
+}
