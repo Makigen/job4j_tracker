9c263e0376ef4e54d6f96e3e520a0d87357b534b
Kirill Malikov <makige.ee@mail.ru>
2020-03-19 23:30:03 +0200

    1. Чтение из консоли. Класс Scanner.[#244528]

---
+++ src/main/java/oop/MagicBall.java
@@ -0,0 +1,20 @@
+package oop;
+
+import java.util.Scanner;
+import java.util.Random;
+
+public class MagicBall {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int answer = new Random().nextInt(3);
+        System.out.print("Я великий Оракул. Что ты хочешь узнать? ");
+        input.nextLine();
+        if (answer == 0) {
+            System.out.println("Да");
+        } else if (answer == 1) {
+            System.out.println("Нет");
+        } else {
+            System.out.println("Может быть");
+        }
+    }
+}
