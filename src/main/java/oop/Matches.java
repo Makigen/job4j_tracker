4751a17593662f3aafe5bc9335a76802f9b93541
Kirill Malikov <makige.ee@mail.ru>
2020-03-20 21:58:07 +0200

    2. Scanner и чтение числа из консоли.[#244529]

--- src/main/java/oop/Matches.java
+++ src/main/java/oop/Matches.java
@@ -12,21 +12,14 @@ public class Matches {
         System.out.println("Welcome to the MATCHES GAME! There are " + matches + " matches on the table." + System.lineSeparator() + "Player can take 1-3 matches in one turn. Player taking the last match wins!");
         while (run && matches > 0) {
             System.out.println("There are " + matches + " matches on the table.");
-            if (count % 2 == 0) {
-                player = 2;
-            } else {
-                player = 1;
-            }
+            player = count % 2 == 0 ?  2 :  1;
             System.out.println("Player " + player + " turn:");
             int turn = Integer.valueOf(input.nextLine());
             if (turn <= 3 && turn >= 1) {
                 matches = matches - turn;
                 count++;
             }
-            if (matches <= 0) {
-                run = false;
-                System.out.println("The winner is Player " + player + "!");
-            }
         }
+        System.out.println("The winner is Player " + player + "!");
     }
 }
