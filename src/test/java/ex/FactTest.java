3a6ff2f361348749e351d3a8dc100daf3035192c
Kirill Malikov <makige.ee@mail.ru>
2020-08-14 22:43:12 +0300

    0.3. Тестирование исключений с junit [#244548]

--- src/test/java/ex/FactTest.java
+++ src/test/java/ex/FactTest.java
@@ -1,12 +1,19 @@
 package ex;
 
 import org.junit.Test;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.*;
 
 public class FactTest {
 
     @Test(expected = IllegalArgumentException.class)
     public void whenLessThan0() {
         Fact.calc(-1);
+    }
+
+    @Test
+    public void when3Than6() {
+        assertThat(Fact.calc(5), is(120));
     }
 }
 
