5f68d4feea74cf33929b8fbbf0bef6b0f3a8485d
Kirill Malikov <makige.ee@mail.ru>
2020-08-15 23:55:12 +0300

    0.5. Иерархия исключений и множественный catch. [#244550]

--- src/main/java/ex/UserStore.java
+++ src/main/java/ex/UserStore.java
@@ -5,15 +5,13 @@ public class UserStore {
         for (User user : users) {
             if (user.getUsername().equals(login)){
                 return user;
-            } else {
-                throw new UserNotFoundException("User not found");
             }
         }
-        return null;
+        throw new UserNotFoundException("User not found");
     }
 
     public static boolean validate(User user) throws UserInvalidException {
-        if (user.getUsername().length() < 3 | !user.isValid()) {
+        if (user.getUsername().length() < 3 || !user.isValid()) {
             throw new UserInvalidException("User is not valid");
         }
         return true;
@@ -26,15 +24,11 @@ public class UserStore {
         User user = null;
         try {
             user = findUser(users, "Kirill Malikov");
-        } catch (UserNotFoundException e) {
-            e.printStackTrace();
-        }
-        try {
-            if (validate(user)) {
-                System.out.println("This user has an access");
-            }
-        } catch (UserInvalidException e) {
-            e.printStackTrace();
+            System.out.println("This user has an access");
+        } catch (UserInvalidException uie) {
+            uie.printStackTrace();
+        } catch (UserNotFoundException unfe) {
+            unfe.printStackTrace();
         }
     }
 }
