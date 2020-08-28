189773f6af4837f0815803ef3668565925a2f466
Kirill Malikov <makige.ee@mail.ru>
2020-08-28 21:15:03 +0300

    3. Конвертация ArrayList в двухмерный массив [#244481]

--- src/main/java/collection/ConvertList2Array.java
+++ src/main/java/collection/ConvertList2Array.java
@@ -10,7 +10,7 @@ public class ConvertList2Array {
         for (Integer num : list) {
             array[row][cell] = num;
             cell++;
-            if (cell == cells && row < groups - 1) {
+            if (cell == cells) {
                 row++;
                 cell = 0;
             }
