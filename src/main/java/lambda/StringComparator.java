1342da86fae7f6acab19f54389540a953ffedda2
Makigen <makige.ee@mail.ru>
2021-04-15 16:36:06 +0300

    2. Функциональные итерфейсы. [#244343]

--- src/main/java/lambda/StringComparator.java
+++ src/main/java/lambda/StringComparator.java
@@ -5,19 +5,15 @@ import java.util.Comparator;
 import java.util.List;
 
 public class StringComparator {
-
-    Comparator<String> cmpText = (left, right) -> left.compareTo(right);
-    
-    Comparator<String> cmpDescSize = (left, right) -> right.length() - left.length();
-
     public static void main(String[] args) {
+        Comparator<String> cmpText = (left, right) -> left.compareTo(right);
+        Comparator<String> cmpDescSize = (left, right) -> Integer.compare(right.length(), left.length());
         String s1 = "Kirill";
         String s2 = "Malikov";
         List<String> names = Arrays.asList(s1, s2);
-        StringComparator stc = new StringComparator();
-        names.sort(stc.cmpText);
+        names.sort(cmpText);
         System.out.println(names);
-        names.sort(stc.cmpDescSize);
+        names.sort(cmpDescSize);
         System.out.println(names);
     }
 }
